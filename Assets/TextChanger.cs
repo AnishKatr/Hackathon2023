e36f832e13c2e4f0168a8268d38f781652dec255
tyler costello <tyc642@gmail.com>
2023-02-18 16:22:21 -0800

    counter working

--- Assets/TextChanger.cs
+++ Assets/TextChanger.cs
@@ -2,20 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 
 
 
 public class TextChanger : MonoBehaviour {
-   int n;
    public TextMeshProUGUI txt;
+   public Button myButton;
    public string replacement;
+   void Start(){
+      Button btn = myButton.GetComponent<Button>();
+
+   }
    public void OnButtonPress(){
       
       txt.text = replacement;
+      myButton.interactable= false;
    }
 }
