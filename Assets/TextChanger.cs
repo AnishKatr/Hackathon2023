9fc7bc9a111396c70b9d5d422b33b98e7cc1d4f8
Anish K <47697221+AnishKatr@users.noreply.github.com>
2023-02-19 10:46:28 -0800

    closing scene

--- Assets/TextChanger.cs
+++ Assets/TextChanger.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
 
@@ -10,6 +11,9 @@ public class TextChanger : MonoBehaviour {
    public TextMeshProUGUI txt;
    public Button myButton;
    public string replacement;
+   public static int count;
+
+
    void Start(){
       Button btn = myButton.GetComponent<Button>();
 
@@ -18,5 +22,22 @@ public class TextChanger : MonoBehaviour {
       
       txt.text = replacement;
       myButton.interactable= false;
+      count++;
+      
+      
    }
+   void Update(){
+      if (Input.GetMouseButtonDown(0)){
+         sceneChanger();
+      }
+   }
+
+   public void sceneChanger(){
+     
+      if(count == 4 ){
+         
+            SceneManager.LoadScene("closeGame");
+      }
+   }
+   
 }
