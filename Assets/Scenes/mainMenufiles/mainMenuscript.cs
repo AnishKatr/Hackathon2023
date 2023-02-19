c6e46753a80a0caa4faa2c2720bbee92fcd885e6
Anish K <47697221+AnishKatr@users.noreply.github.com>
2023-02-18 17:07:31 -0800

    commentedoutscript

--- Assets/Scenes/mainMenufiles/mainMenuscript.cs
+++ Assets/Scenes/mainMenufiles/mainMenuscript.cs
@@ -2,31 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mainMenuscript : MonoBehaviour
-{
-    // Chapter 1
-    public void loadChapter1(){
+// public class mainMenuscript : MonoBehaviour
+// {
+//     // Chapter 1
+//     public void loadChapter1(){
         
-        SceneManager.LoadScene("Chapter1_OCD");
+//         SceneManager.LoadScene("Chapter1_OCD");
 
-    }
+//     }
 
-    //Chapter 2
-    public void loadChapter1(){
+//     //Chapter 2
+//     public void loadChapter1(){
         
-        SceneManager.LoadScene("Chapter2");
+//         SceneManager.LoadScene("Chapter2");
 
-    }
+//     }
 
-    public void home(){
+//     public void home(){
 
-        SceneManager.LoadScene("mainMenu");
+//         SceneManager.LoadScene("mainMenu");
 
-    }
+//     }
 
-    public void quitGame(){
+//     public void quitGame(){
 
-        Application.Quit();
+//         Application.Quit();
 
-    }
-}
+//     }
+// }
