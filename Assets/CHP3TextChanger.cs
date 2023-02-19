0d8e43ff6468133348355ae04a7c79a357560af4
Anish K <47697221+AnishKatr@users.noreply.github.com>
2023-02-19 11:00:43 -0800

    more fixesssss

---
+++ Assets/CHP3TextChanger.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+
+
+public class CHP3TextChanger : MonoBehaviour {
+   public TextMeshProUGUI txt;
+   public Button myButton;
+   public string replacement;
+   public static int count;
+
+
+   void Start(){
+      Button btn = myButton.GetComponent<Button>();
+
+   }
+   public void OnButtonPress(){
+      
+      txt.text = replacement;
+      myButton.interactable= false;
+      count++;
+      
+      
+   }
+   void Update(){
+      if (Input.GetMouseButtonDown(0)){
+         sceneChanger();
+      }
+   }
+
+   public void sceneChanger(){
+     
+      if(count == 4 ){
+         
+            SceneManager.LoadScene("closeGame");
+      }
+   }
+   
+}
